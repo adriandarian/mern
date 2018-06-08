a163296b1b2d29f86f5e270c04a8b9440b755f30
Adrian Darian <adarian@ucmerced.edu>
2018-06-07 22:07:58 -0700

    client package

---
+++ client/app/components/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+const App = ({ children }) => (
+  <div>
+  <Header />
+  <main>
+    {children}
+  </main>
+
+  <Footer />
+  </div>
+);
+
+export default App;
